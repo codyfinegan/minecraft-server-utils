62d7a9d6bb0f9ffe58d6cc47e32c2d3178ee25c2
Cody Finegan <44886+codyfinegan@users.noreply.github.com>
2022-03-20 17:06:22 +1300

    Adding S3 syncing for backups

--- daily-snapshot.sh
+++ daily-snapshot.sh
@@ -2,8 +2,11 @@
 
 FROM="$MCSU_SOURCEDIR/"
 TO="$MCSU_TARGETDIR/${MCSU_SOURCEDIR_BASENAME}_$(date +%F)"
+ZIP="$MCSU_TARGETDIR/${MCSU_SOURCEDIR_BASENAME}_$(date +%F).tar.gz"
 
 mkdir -p "$TO"
 rsync -xavh --delete "$FROM" "$TO"
 
+tar -czf $ZIP $TO
+aws s3 sync --exclude='*' --include='*.tar.gz' ${MCSU_TARGETDIR} s3://${MCSU_S3_BUCKET}/${MCSU_SOURCEDIR_BASENAME} --delete 
 
--- purge-snapshots.sh
+++ purge-snapshots.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# keep all but the last 5 aws zips
+echo "Pruning tar files"
+(cd $MCSU_TARGETDIR && ls -tp | grep -v '/$' | tail -n +5 | xargs -I {} rm -- {})
+echo "Syncing to s3"
+aws s3 sync --exclude='*' --include='*.tar.gz' ${MCSU_TARGETDIR} s3://${MCSU_S3_BUCKET}/${MCSU_SOURCEDIR_BASENAME} --delete
+
+
+echo "Processing folders"
 REQUIRED_FREESPACE="$(du -s "$MCSU_SOURCEDIR" | cut -f1)"
 
 ls -1rt "$MCSU_TARGETDIR" | grep - | while read -r f; do
