95acba79eaf9ce86fcbde06f396b2454038948b8
Cody Finegan <44886+codyfinegan@users.noreply.github.com>
2022-09-25 10:57:58 +1300

    Updating restart script

--- timed-restart.sh
+++ timed-restart.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 # via http://stackoverflow.com/a/34614432
-function exec_in_screen {
+function start_screen {
     name=$1
     command=$2
     screen -dmS $name sh; sleep 1; screen -S $name -X stuff "$command\n";
+}
+
+function exec_in_screen {
+    name=$1
+    command=$2
+    screen -S $name -X stuff "$command\n";
 }
 
 # 10 mins
@@ -53,8 +59,17 @@ exec_in_screen "mcs" "/tellraw @a {\"text\":\"Server is restarting now!\",\"ital
 echo "Stoppings service"
 exec_in_screen "mcs" "/stop"
 
-# Wait 1 minute to restart
-echo "Waiting for service to stop for 60 seconds"
-sleep 60
-screen -ls
-# exec_in_screen "mcs" "$MCSU_SERVERCMD"
+# Wait for 10 seconds
+sleep 10
+
+# Wait for minecraft to finish
+while pgrep java > /dev/null; do sleep 2; echo 'Waiting for server to stop...'; done
+
+# Ok, now kill the screen service
+exec_in_screen "mcs" "^D"
+
+# Confirm screen finished
+while pgrep java > /dev/null; do sleep 2; echo 'Waiting for screen to stop...'; done
+
+echo "Starting server..."
+start_screen "mcs" "$MCSU_SERVERCMD"
