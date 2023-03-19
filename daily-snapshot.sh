94ce4349d71667a5f010af087b9bc72c98bde3a4
Cody Finegan <44886+codyfinegan@users.noreply.github.com>
2023-03-19 00:14:20 +0000

    Partial paths

--- daily-snapshot.sh
+++ daily-snapshot.sh
@@ -7,6 +7,5 @@ ZIP="$MCSU_TARGETDIR/${MCSU_SOURCEDIR_BASENAME}_$(date +%F).tar.gz"
 mkdir -p "$TO"
 rsync -xavh --delete "$FROM" "$TO"
 
-tar -czf $ZIP $TO
+tar -czf $ZIP -C $TO .
 aws s3 sync --exclude='*' --include='*.tar.gz' ${MCSU_TARGETDIR} s3://${MCSU_S3_BUCKET}/${MCSU_SOURCEDIR_BASENAME} --delete 
-
