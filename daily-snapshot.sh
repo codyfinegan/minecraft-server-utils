1e93814a2995f61e8acc783f5d336345e5208dd2
Cody Finegan <44886+codyfinegan@users.noreply.github.com>
2023-03-19 00:24:59 +0000

    Fixing up backups

--- daily-snapshot.sh
+++ daily-snapshot.sh
@@ -2,11 +2,11 @@
 
 PREFIX=""
 FROM="$MCSU_SOURCEDIR/"
-TO="$MCSU_TARGETDIR/${PREFIX}${MCSU_SOURCEDIR_BASENAME}_$(date +%F)"
-ZIP="$MCSU_TARGETDIR/${PREFIX}${MCSU_SOURCEDIR_BASENAME}_$(date +%F).tar.gz"
+TO="$MCSU_TARGETDIR/${PREFIX}${MCSU_SOURCEDIR_BASENAME}${MCSU_VER}_$(date +%F)"
+ZIP="$MCSU_TARGETDIR/${PREFIX}${MCSU_SOURCEDIR_BASENAME}${MCSU_VER}_$(date +%F).tar.gz"
 
 mkdir -p "$TO"
 rsync -xavh --delete "$FROM" "$TO"
 
 tar -czf $ZIP -C $TO .
-aws s3 sync --exclude='*' --include='*.tar.gz' ${MCSU_TARGETDIR} s3://${MCSU_S3_BUCKET}/${MCSU_SOURCEDIR_BASENAME} --delete 
+aws s3 sync --exclude='*' --include='*.tar.gz' ${MCSU_TARGETDIR} s3://${MCSU_S3_BUCKET}/${MCSU_SOURCEDIR} --delete 
--- incremental-backup.sh
+++ incremental-backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 FROM="$MCSU_SOURCEDIR/"
-TO="$MCSU_TARGETDIR/${MCSU_SOURCEDIR_BASENAME}"
+TO="$MCSU_TARGETDIR/${MCSU_SOURCEDIR_BASENAME}${MCSU_VER}"
 
 mkdir -p "$TO"
 rsync -xavh --delete "$FROM" "$TO"
